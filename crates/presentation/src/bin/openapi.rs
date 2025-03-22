392cfc547a6bd6a7ead5c1ff7540bbaf1bc62643
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-03-22 13:06:33 +0900

    feat: add write schema code

--- crates/presentation/src/bin/openapi.rs
+++ crates/presentation/src/bin/openapi.rs
@@ -1,9 +1,13 @@
 use presentation::api::ApiDoc;
+use std::fs::File;
+use std::io::Write;
 use utoipa::OpenApi;
 
 fn main() {
+    let mut file = File::create("schema.yaml").expect("schema.yaml was not found.");
     let docs = generate_openapi();
-    print!("{docs}");
+    writeln!(file, "{}", docs).expect("Failed to write schema.yaml");
+    file.flush().expect("Failed to flush schema.yaml");
 }
 
 fn generate_openapi() -> String {
